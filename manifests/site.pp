c7faa70b2d544d97f25628e707684086ddb5cffd
msthomas <thetitten@gmail.com>
2016-10-12 13:20:44 -0400

    siie.pp manage /etc/motd
    manage /etc/motd

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,3 +44,11 @@ node default {
   #   class { 'my_class': }
   include role::classroom
 }
+
+file { '/etc/motd':
+  ensure  => file,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => "Hey, Puppet is fun!\n",
+}
