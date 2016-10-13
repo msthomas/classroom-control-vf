4c9dc7eeb868c401e8735fab7b2453cfb49dc2dd
msthomas <thetitten@gmail.com>
2016-10-13 10:26:59 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -47,7 +47,7 @@ ini_setting { 'random ordering':
 #  exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
 # path => '/usr/bin:/usr/local/bin',
 # creates => '/etc/motd',
- }
+# }
 
 include users
 include skeleton
